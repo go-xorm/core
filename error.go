c513c134ef47a6e636f648dba13aaad4ef3b4f51
Lunny Xiao <xiaolunwen@gmail.com>
2014-05-09 21:52:51 +0800

    remove unused codes

--- error.go
+++ error.go
@@ -5,6 +5,6 @@ import "errors"
 var (
 	ErrNoMapPointer    = errors.New("mp should be a map's pointer")
 	ErrNoStructPointer = errors.New("mp should be a map's pointer")
-	ErrNotExist        = errors.New("Not exist")
-	ErrIgnore          = errors.New("Ignore")
+	//ErrNotExist        = errors.New("Not exist")
+	//ErrIgnore = errors.New("Ignore")
 )
